fdf6fceb4c928d487d0f057eb0c9d203ca51ef04
freehere107 <freehere107@hotmail.com>
2019-11-11 11:48:21 +0800

    update metadata reg

--- codec/tools.py
+++ codec/tools.py
@@ -1,47 +1,57 @@
 import json
-
+import os
 from scalecodec import ScaleBytes
-from scalecodec.base import ScaleDecoder
+from scalecodec.base import ScaleDecoder, Singleton
 from scalecodec.block import ExtrinsicsDecoder, EventsDecoder, LogDigest
+from codec import MetadataInstant
 
 from libs import rpc_pb2, rpc_pb2_grpc
-from codec import MetadataV6Instant, MetadataKusamaInstant, MetadataKusama1006Instant, \
-    MetadataKusama1008Instant, MetadataKusama1010Instant, MetadataKusama1011Instant, MetadataKusama1012Instant, \
-    MetadataKusama1015Instant, MetadataKusama1016Instant
+
+
+class MetadataRegistry(metaclass=Singleton):
+    registry = {}
+    network = os.getenv("NETWORK_NODE", "darwinia")
+
+    @classmethod
+    def all_subclasses(cls, class_):
+        return set(class_.__subclasses__()).union(
+            [s for c in class_.__subclasses__() for s in cls.all_subclasses(c)])
+
+    def __init__(self):
+        self.registry = ({cls.__name__.lower(): cls for cls in self.all_subclasses(MetadataInstant)})
+
+    @classmethod
+    def get_class_decoder(cls, spec):
+        cl = MetadataRegistry().registry.get("%s%dinstant" % (MetadataRegistry().network, spec), None)
+        return cl().Decoder
+
+    @classmethod
+    def has_reg(cls, spec):
+        class_string = "%s%dinstant" % (MetadataRegistry().network, spec)
+        return class_string in MetadataRegistry().registry
 
 
 class Tools(rpc_pb2_grpc.ToolsServicer):
-    MetadataDecoderInstantMap = {
-        78: MetadataV6Instant(),
-        1003: MetadataKusamaInstant(),
-        1006: MetadataKusama1006Instant(),
-        1008: MetadataKusama1008Instant(),
-        1010: MetadataKusama1010Instant(),
-        1011: MetadataKusama1011Instant(),
-        1012: MetadataKusama1012Instant(),
-        1015: MetadataKusama1015Instant(),
-        1016: MetadataKusama1016Instant(),
-    }
 
     def DecodeExtrinsic(self, request, context):
         msg = json.loads(request.message)
-        metadata_version = request.metadataVersion
-        while metadata_version not in self.MetadataDecoderInstantMap:
-            metadata_version -= 1
-        m = self.MetadataDecoderInstantMap[metadata_version]
+        spec_ver = request.metadataVersion
+        while MetadataRegistry.has_reg(spec_ver) is False:
+            spec_ver -= 1
+        t = MetadataRegistry.get_class_decoder(spec_ver)
         result = []
         for idx, extrinsic_data in enumerate(msg):
-            extrinsic_decoder = ExtrinsicsDecoder(data=ScaleBytes(extrinsic_data), metadata=m.Decoder)
+            extrinsic_decoder = ExtrinsicsDecoder(data=ScaleBytes(extrinsic_data), metadata=t)
             result.append(extrinsic_decoder.decode(False))
         return rpc_pb2.ExtrinsicReply(message=json.dumps(result))
 
     def DecodeEvent(self, request, context):
         event = request.message
-        metadata_version = request.metadataVersion
-        while metadata_version not in self.MetadataDecoderInstantMap:
-            metadata_version -= 1
-        m = self.MetadataDecoderInstantMap[metadata_version]
-        events_decoder = EventsDecoder(data=ScaleBytes(event), metadata=m.Decoder)
+        spec_ver = request.metadataVersion
+        while MetadataRegistry.has_reg(spec_ver) is False:
+            spec_ver -= 1
+        t = MetadataRegistry.get_class_decoder(spec_ver)
+        events_decoder = EventsDecoder(data=ScaleBytes(event), metadata=t)
         return rpc_pb2.EventReply(message=json.dumps(events_decoder.decode(False)))
 
     def DecodeLog(self, request, context):
