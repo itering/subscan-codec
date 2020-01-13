068ea534167d82f7460cc40ee935734ec7e8752e
freehere107 <freehere107@hotmail.com>
2020-01-13 10:16:16 +0800

    update grpc version

--- server.py
+++ server.py
@@ -1,31 +1,22 @@
 import os
-
-import time
-from concurrent import futures
-
 import grpc
-from scalecodec.base import RuntimeConfiguration
 
+from concurrent import futures
+from scalecodec.base import RuntimeConfiguration
 from codec.tools import Tools
 from libs import rpc_pb2_grpc
 from type_registry import load_type_registry
 from logger.conf import log_config, LOGGER
 from codec.tools import MetadataRegistry
 
-_ONE_DAY_IN_SECONDS = 60 * 60 * 24
-
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), compression=grpc.Compression.Gzip)
     rpc_pb2_grpc.add_ToolsServicer_to_server(Tools(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     LOGGER.info("SubScan Tools server start :50051")
-    try:
-        while True:
-            time.sleep(_ONE_DAY_IN_SECONDS)
-    except KeyboardInterrupt:
-        server.stop(0)
+    server.wait_for_termination()
 
 
 def type_registry():
